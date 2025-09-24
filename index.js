036a0f88b0dc1d458f9e66a7836ab98cc38d3ec5
Shio-T0 <ShiroTomo@proton.me>
2025-09-24 14:11:31 +0100

    first-commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+let first_catet = document.getElementById("catet1");
+let second_catet = document.getElementById("catet2");
+
+let result = Math.sqrt(Number(first_catet)**2 + Number(second_catet)**2);
+
+document.getElementById("result").content = result;
